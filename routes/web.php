8b4cd49b90ed9ded2167922bfa1494d51e4698f8
ekopram <ekopram9@yahoo.co.id>
2023-03-13 17:17:51 +0700

    View Login & Registration
    membuat view login, atur css, router, hapus class router yg g perlu,

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 use App\Models\Category;
-use App\Models\User;
+use App\Http\Controllers\LoginController;
 
 
 /*
@@ -53,6 +52,7 @@ Route::get('/categories', function () {
     ]);
 });
 
+Route::get('/login', [LoginController::class, 'index']);
 
 
 
