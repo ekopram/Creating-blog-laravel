30d3b23d8cfa9ba1b7788aa7cf5df620dbb8f085
ekopram <ekopram9@yahoo.co.id>
2023-03-14 15:36:26 +0700

    User Login & Middleware
    middleware,buat login,dasboard harus login,penamaan router,

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -20,11 +20,35 @@
                     <a class="nav-link {{ $active === 'Categories' ? 'active' : '' }}" href="/categories">Categories</a>
                 </li>
             </ul>
+
             <ul class="navbar-nav ms-auto">
+                @auth
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Welcome back, {{ auth()->user()->name }}
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-sidebar-reverse"></i>
+                                    My
+                                    Dashboard</a></li>
+                            <hr class="dropdown-divider">
+                    </li>
+                    <li>
+                        <form action="/logout" method="post">
+                            @csrf
+                            <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i>
+                                Logout</a></button>
+                        </form>
+                    </li>
+                </ul>
+                </li>
+            @else
                 <li class="nav-item">
                     <a href="/login" class="nav-link {{ $active === 'home' ? 'active' : '' }}"><i
                             class="bi bi-box-arrow-in-right"></i> Login</a>
                 </li>
+            @endauth
             </ul>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,8 @@ use App\Http\Controllers\DashboardController;
 
 // menit 27:07
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('home', [
         "title" => "home",
         'active' => 'home'
@@ -55,13 +52,14 @@ Route::get('/categories', function () {
     ]);
 });
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
 
-Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
 
 
 
