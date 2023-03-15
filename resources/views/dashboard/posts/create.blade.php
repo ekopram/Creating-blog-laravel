7647d843dea0a92307cf77ef679706384ff865e3
ekopram <ekopram9@yahoo.co.id>
2023-03-15 12:01:37 +0700

    Create Post Form
    combo box category, buat editor dr trix editor, nonaktifkan fitur upload image, download css dan js trix ke folder, buat postingan

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -6,7 +6,7 @@
     </div>
 
     <div class="col-lg-8">
-        <form method="post" cation="/dashboard/posts">
+        <form method="post" action="/dashboard/posts">
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
@@ -15,6 +15,19 @@
             <div class="mb-3">
                 <label for="slug" class="form-label">Slug</label>
                 <input type="text" class="form-control" id="slug" name="slug" disable readonly>
+            </div>
+            <div class="mb-3">
+                <label for="category" class="form-label">Category</label>
+                <select class="form-select" name="category_id">
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="body" class="form-label">Body</label>
+                <input id="body" type="hidden" name="body">
+                <trix-editor input="body"></trix-editor>
             </div>
 
             <button type="submit" class="btn btn-primary">Create Post</button>
@@ -30,5 +43,9 @@
                 .then(respone => respone.json())
                 .then(data => slug.value = data.slug)
         });
+
+        document.addEventListener('trix-file-accept', function(e) {
+            e.preventDefault();
+        })
     </script>
 @endsection
